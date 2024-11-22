934ee034ae5fe57522c752ee676cea22f8958af7
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-11-22 09:53:09 +0900

    feat: 라벨 서비스 인터페이스 추가

---
+++ src/main/java/postman/bottler/label/controller/LabelService.java
@@ -0,0 +1,9 @@
+package postman.bottler.label.controller;
+
+public interface LabelService {
+    void createLabel(String imageUrl);
+    void findDefaultLabels();
+    void findAllLabels();
+    void findOneLabel(Long letterId);
+    void findUserLabels(Long userId);
+}
--- src/main/java/postman/bottler/label/service/LabelService.java
+++
@@ -1,5 +0,0 @@
-package postman.bottler.label.service;
-
-public interface LabelService {
-    //라벨 url 요청
-}
