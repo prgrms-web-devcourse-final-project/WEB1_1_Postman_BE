544664d2c9b71bb797ae9da1a124eade8df992b1
l2yuPa <jeungwon28@gmail.com>
2024-11-22 11:31:37 +0900

    feat: LetterService 블락 기능 추가

--- src/main/java/postman/bottler/letter/controller/LetterService.java
+++ src/main/java/postman/bottler/letter/controller/LetterService.java
@@ -7,7 +7,8 @@ import postman.bottler.letter.dto.request.LetterRequestDTO;
 import java.util.List;
 
 public interface LetterService {
-    //편지 블락 처리 로직
+    //편지 블락 처리 로직 편지 블락 시키고 3번 되면 정지요청만 보내기
+    void incrementWarningCount(Long letterId);
     Letter createLetter(LetterRequestDTO request);
     List<Letter> getSentLetters(Pageable pageable);
     Letter getLetterDetail(Long letterId);
