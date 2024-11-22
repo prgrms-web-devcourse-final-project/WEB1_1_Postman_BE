eaadbc3f1e0596501f2351f810665bfdfa4a271d
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-11-22 16:57:31 +0900

    feat: 라벨 저장 기능에 필요한 LabelServiceImpl 추가

--- src/main/java/postman/bottler/label/service/LabelServiceImpl.java
+++ src/main/java/postman/bottler/label/service/LabelServiceImpl.java
@@ -1,4 +1,34 @@
 package postman.bottler.label.service;
 
-public class LabelServiceImpl {
+import org.springframework.stereotype.Service;
+import postman.bottler.label.controller.LabelService;
+import postman.bottler.label.domain.Label;
+
+@Service
+public class LabelServiceImpl implements LabelService {
+    private final LabelRepository labelRepository;
+
+    public LabelServiceImpl(LabelRepository labelRepository) {
+        this.labelRepository = labelRepository;
+    }
+
+    @Override
+    public void createLabel(String imageUrl) {
+        labelRepository.save(Label.createLabel(imageUrl));
+    }
+
+    @Override
+    public void findAllLabels() {
+
+    }
+
+    @Override
+    public void findOneLabel(Long letterId) {
+
+    }
+
+    @Override
+    public void findUserLabels(Long userId) {
+
+    }
 }
