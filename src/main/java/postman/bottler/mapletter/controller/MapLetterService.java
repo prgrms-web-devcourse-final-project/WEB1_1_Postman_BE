d7c649700b94bb30b6d1a4d8ae5dac223e3cd0e0
kahyun0255 <kkh9946@naver.com>
2024-11-22 10:41:03 +0900

    refactor: #7 메서드명 수정

--- src/main/java/postman/bottler/mapletter/controller/MapLetterService.java
+++ src/main/java/postman/bottler/mapletter/controller/MapLetterService.java
@@ -1,5 +1,5 @@
 package postman.bottler.mapletter.controller;
 
 public interface MapLetterService {
-    void incrementWarningCount(Long mapLetterId); //블락 카운트 증가
+    void blockMapLetter(Long mapLetterId); //지도 편지 블락시키는 메서드
 }
