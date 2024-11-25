5afcc5eab3ac641b087938514910bdc71c4169f7
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-11-25 12:26:16 +0900

    test: Label 도메인 테스트 - label response dto 리스트를 label 도메인 리스트로 변환한다.

--- src/test/java/postman/bottler/label/domain/LabelTest.java
+++ src/test/java/postman/bottler/label/domain/LabelTest.java
@@ -2,9 +2,11 @@ package postman.bottler.label.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import postman.bottler.label.dto.response.LabelResponseDTO;
 
 @SpringBootTest
 class LabelTest {
@@ -20,5 +22,21 @@ class LabelTest {
         // then
         assertNotNull(label, "Label은 null이 될 수 없음");
         assertEquals(imageUrl, label.getImageUrl(), "주어진 url로 Label 생성");
+    }
+
+    @Test
+    @DisplayName("label response dto 리스트를 label 도메인 리스트로 변환한다.")
+    void toResponseDTOList() {
+        // given
+        Label label1 = Label.createLabel("http://example.com/image1.png");
+        Label label2 = Label.createLabel("http://example.com/image2.png");
+        List<Label> labels = List.of(label1, label2);
+
+        // when
+        List<LabelResponseDTO> dtos = Label.toResponseDTOList(labels);
+
+        // then
+        assertNotNull(dtos, "Response DTO 리스트는 null이 될 수 없음");
+        assertEquals(2, dtos.size(), "DTO 리스트 사이즈는 2");
     }
 }
