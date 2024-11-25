41e86fd3fc8c089480903a617b4a816f60c9f7d9
kahyun0255 <kkh9946@naver.com>
2024-11-25 10:42:40 +0900

    refactor: #2 패키지 구조 수정. service interface 삭제

--- src/main/java/postman/bottler/mapletter/service/MapLetterService.java
+++ src/main/java/postman/bottler/mapletter/service/MapLetterService.java
@@ -3,37 +3,28 @@ package postman.bottler.mapletter.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import postman.bottler.global.exception.CommonForbiddenException;
-import postman.bottler.mapletter.controller.MapLetterService;
 import postman.bottler.mapletter.domain.MapLetter;
 import postman.bottler.mapletter.domain.MapLetterType;
 import postman.bottler.mapletter.dto.request.CreatePublicMapLetterRequestDTO;
 import postman.bottler.mapletter.dto.request.CreateTargetMapLetterRequestDTO;
 import postman.bottler.mapletter.dto.response.OneLetterResponse;
-import postman.bottler.user.service.UserService;
-
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class MapLetterServiceImpl implements MapLetterService {
+public class MapLetterService {
     private final MapLetterRepository mapLetterRepository;
 //    private final UserService userService;
 
-    @Override
     public MapLetter createPublicMapLetter(CreatePublicMapLetterRequestDTO createPublicMapLetterRequestDTO, Long userId) {
         MapLetter mapLetter = MapLetter.createPublicMapLetter(createPublicMapLetterRequestDTO, userId);
         return mapLetterRepository.save(mapLetter);
     }
 
-    @Override
     public MapLetter createTargetMapLetter(CreateTargetMapLetterRequestDTO createTargetMapLetterRequestDTO, Long userId) {
         MapLetter mapLetter = MapLetter.createTargetMapLetter(createTargetMapLetterRequestDTO, userId);
         return mapLetterRepository.save(mapLetter);
     }
 
-    @Override
     public OneLetterResponse findOneMepLetter(Long letterId, Long userId) {
         MapLetter mapLetter = mapLetterRepository.findById(letterId);
         if (mapLetter.getType() == MapLetterType.PRIVATE && !mapLetter.getTargetUserId().equals(userId)) {
