20ab762beac846d8b09b0095833f7c8aa8cb0002
l2yuPa <jeungwon28@gmail.com>
2024-11-25 17:24:02 +0900

    feat: LetterController에 보낸 키워드 편지 삭제 기능 추가

--- src/main/java/postman/bottler/letter/controller/LetterController.java
+++ src/main/java/postman/bottler/letter/controller/LetterController.java
@@ -2,7 +2,9 @@ package postman.bottler.letter.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,5 +37,11 @@ public class LetterController {
     ) {
         Page<LetterKeywordsResponseDTO> result = letterService.getLetterKeywords(page, size, sort);
         return ApiResponse.onSuccess(result);
+    }
+
+    @DeleteMapping("/{letterId}")
+    public ApiResponse<String> deleteLetter(@PathVariable Long letterId) {
+        letterService.deleteLetter(letterId);
+        return ApiResponse.onSuccess("success");
     }
 }
