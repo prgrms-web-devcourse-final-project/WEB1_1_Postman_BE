03401623ebdd94d16f5d2b750dd046b882ec5d2d
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-11-25 10:33:20 +0900

    refactor: service interface 제거

--- src/main/java/postman/bottler/label/controller/LabelController.java
+++ src/main/java/postman/bottler/label/controller/LabelController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import postman.bottler.global.response.ApiResponse;
 import postman.bottler.label.dto.response.LabelResponseDTO;
+import postman.bottler.label.service.LabelService;
 
 @RestController
 @RequestMapping("/labels")
--- src/main/java/postman/bottler/label/service/LabelService.java
+++ src/main/java/postman/bottler/label/service/LabelService.java
@@ -1,39 +1,24 @@
 package postman.bottler.label.service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
-import postman.bottler.label.controller.LabelService;
 import postman.bottler.label.domain.Label;
 import postman.bottler.label.dto.response.LabelResponseDTO;
-import postman.bottler.label.infra.entity.LabelEntity;
 
 @Service
-public class LabelServiceImpl implements LabelService {
+public class LabelService {
     private final LabelRepository labelRepository;
 
-    public LabelServiceImpl(LabelRepository labelRepository) {
+    public LabelService(LabelRepository labelRepository) {
         this.labelRepository = labelRepository;
     }
 
-    @Override
     public void createLabel(String imageUrl) {
         labelRepository.save(Label.createLabel(imageUrl));
     }
 
-    @Override
     public List<LabelResponseDTO> findAllLabels() {
         List<Label> labels =  labelRepository.findAllLabels();
         return Label.toResponseDTOList(labels);
-    }
-
-    @Override
-    public void findOneLabel(Long letterId) {
-
-    }
-
-    @Override
-    public void findUserLabels(Long userId) {
-
     }
 }
