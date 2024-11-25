d31f2a40efd96bbe6db521ab38d3543126b87c77
hgh1472 <hgh1472@naver.com>
2024-11-26 00:23:17 +0900

    refactor: 트랜잭션 처리 추가

--- src/main/java/postman/bottler/notification/service/NotificationService.java
+++ src/main/java/postman/bottler/notification/service/NotificationService.java
@@ -3,6 +3,7 @@ package postman.bottler.notification.service;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import postman.bottler.notification.domain.Notification;
 import postman.bottler.notification.dto.response.NotificationResponseDTO;
 
@@ -11,18 +12,19 @@ import postman.bottler.notification.dto.response.NotificationResponseDTO;
 public class NotificationService {
     private final NotificationRepository notificationRepository;
 
+    @Transactional
     public NotificationResponseDTO sendNotification(String type, Long userId, Long letterId) {
         Notification notification = Notification.create(type, userId, letterId);
         // TODO 푸시 알림 보내는 로직 추가
         return NotificationResponseDTO.from(notificationRepository.save(notification));
     }
 
+    @Transactional
     public List<NotificationResponseDTO> getUserNotifications(Long userId) {
         List<Notification> notifications = notificationRepository.findByReceiver(userId);
         List<NotificationResponseDTO> result = notifications.stream()
                 .map(NotificationResponseDTO::from)
                 .toList();
-        notifications.forEach(Notification::read);
         notifications.stream()
                 .filter(notification -> !notification.getIsRead())
                 .forEach(Notification::read);
