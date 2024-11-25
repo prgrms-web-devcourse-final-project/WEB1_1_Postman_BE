9f27d12f4639ea0e577601411fa111a4282c4a2b
l2yuPa <jeungwon28@gmail.com>
2024-11-25 17:31:57 +0900

    feat: LetterController에 키워드 편지 보관 기능 추가

--- src/main/java/postman/bottler/letter/controller/LetterController.java
+++ src/main/java/postman/bottler/letter/controller/LetterController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -49,5 +50,11 @@ public class LetterController {
     public ApiResponse<LetterResponseDTO> getLetter(@PathVariable Long letterId) {
         LetterResponseDTO result = letterService.getLetterDetail(letterId);
         return ApiResponse.onSuccess(result);
+    }
+
+    @PutMapping("/{letterId}/save")
+    public ApiResponse<String> saveLetter(@PathVariable Long letterId) {
+        letterService.saveLetter(letterId);
+        return ApiResponse.onSuccess("success");
     }
 }
