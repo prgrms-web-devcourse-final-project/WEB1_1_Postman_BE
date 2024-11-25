d02f3f0f989a33a552e7d31f349128821dfd6f2d
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-11-25 17:07:25 +0900

    refactor: 중복 라벨 에러 코드 변경 - LABEL400 -> LABEL4001

--- src/main/java/postman/bottler/global/response/code/ErrorStatus.java
+++ src/main/java/postman/bottler/global/response/code/ErrorStatus.java
@@ -13,7 +13,7 @@ public enum ErrorStatus {
     FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403"),
 
     // 라벨 에러
-    INVALID_LABEL(HttpStatus.BAD_REQUEST, "LABEL400"),
+    INVALID_LABEL(HttpStatus.BAD_REQUEST, "LABEL4000"),
     EMPTY_LABEL_INPUT(HttpStatus.BAD_REQUEST, "LABEL4001");
 
     private final HttpStatus httpStatus;
