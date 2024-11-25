d994428dc6c54a72fc853a7e79749a3e6ca2c30b
l2yuPa <jeungwon28@gmail.com>
2024-11-25 17:23:33 +0900

    feat: LetterController에 보낸 키워드 편지 조회 기능 추가

--- src/main/java/postman/bottler/letter/controller/LetterController.java
+++ src/main/java/postman/bottler/letter/controller/LetterController.java
@@ -1,12 +1,16 @@
 package postman.bottler.letter.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import postman.bottler.global.response.ApiResponse;
 import postman.bottler.letter.dto.request.LetterRequestDTO;
+import postman.bottler.letter.dto.response.LetterKeywordsResponseDTO;
 import postman.bottler.letter.dto.response.LetterResponseDTO;
 import postman.bottler.letter.service.LetterService;
 
@@ -21,5 +25,15 @@ public class LetterController {
     public ApiResponse<LetterResponseDTO> createLetter(@RequestBody LetterRequestDTO letterRequestDTO) {
         LetterResponseDTO result = letterService.createLetter(letterRequestDTO);
         return ApiResponse.onCreateSuccess(result);
+    }
+
+    @GetMapping("/sent")
+    public ApiResponse<Page<LetterKeywordsResponseDTO>> getLetterKeywords(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "9") int size,
+            @RequestParam(required = false, defaultValue = "createdDate") String sort
+    ) {
+        Page<LetterKeywordsResponseDTO> result = letterService.getLetterKeywords(page, size, sort);
+        return ApiResponse.onSuccess(result);
     }
 }
