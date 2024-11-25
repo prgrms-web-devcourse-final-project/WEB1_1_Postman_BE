58869856642da2134bcd49c72a6da60b68513631
l2yuPa <jeungwon28@gmail.com>
2024-11-25 17:21:00 +0900

    feat: LetterController에 키워드 편지 생성 기능 추가

--- src/main/java/postman/bottler/letter/controller/LetterController.java
+++ src/main/java/postman/bottler/letter/controller/LetterController.java
@@ -1,5 +1,25 @@
 package postman.bottler.letter.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import postman.bottler.global.response.ApiResponse;
+import postman.bottler.letter.dto.request.LetterRequestDTO;
+import postman.bottler.letter.dto.response.LetterResponseDTO;
+import postman.bottler.letter.service.LetterService;
+
+@RestController
+@RequestMapping("/letters")
+@RequiredArgsConstructor
 public class LetterController {
 
+    private final LetterService letterService;
+
+    @PostMapping
+    public ApiResponse<LetterResponseDTO> createLetter(@RequestBody LetterRequestDTO letterRequestDTO) {
+        LetterResponseDTO result = letterService.createLetter(letterRequestDTO);
+        return ApiResponse.onCreateSuccess(result);
+    }
 }
