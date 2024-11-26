43cb0f596e83a985e69db8522070b69aad130156
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-11-26 17:29:37 +0900

    feat: LabelEntity에 ownedCount 추가 및 제약조건 설정 - ownedCount : 해당 라벨을 소유한 유저 수

--- src/main/java/postman/bottler/label/infra/entity/LabelEntity.java
+++ src/main/java/postman/bottler/label/infra/entity/LabelEntity.java
@@ -18,10 +18,15 @@ public class LabelEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long labelId;
 
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     private String imageUrl;
 
+    @Column(nullable = false)
     private int limitCount;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private int ownedCount = 0;
 
     public static LabelEntity from(Label label) {
         return LabelEntity.builder()
@@ -34,6 +39,10 @@ public class LabelEntity {
         return entities.stream()
                 .map(LabelEntity::to)
                 .toList();
+    }
+
+    public static Label toLabel(LabelEntity labelEntity) {
+        return labelEntity.to();
     }
 
     public Label to() {
