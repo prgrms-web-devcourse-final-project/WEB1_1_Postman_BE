2998abcfc0797b8de146e3dd956d69da22e02a19
hgh1472 <hgh1472@naver.com>
2024-11-27 15:36:48 +0900

    refactor: 예외 처리 시, 로그 기록하도록 수정

--- src/main/java/postman/bottler/notification/exception/NotificationExceptionHandler.java
+++ src/main/java/postman/bottler/notification/exception/NotificationExceptionHandler.java
@@ -1,25 +1,30 @@
 package postman.bottler.notification.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import postman.bottler.global.response.ApiResponse;
 import postman.bottler.global.response.code.ErrorStatus;
 
 @RestControllerAdvice
+@Slf4j
 public class NotificationExceptionHandler {
 
     @ExceptionHandler(InvalidNotificationRequestException.class)
     public ApiResponse<?> handleInvalidNotificationRequestException(InvalidNotificationRequestException e) {
+        log.error(e.getMessage());
         return ApiResponse.onFailure(ErrorStatus.INVALID_NOTIFICATION_CREATION.getCode(), e.getMessage(), null);
     }
 
     @ExceptionHandler(NoLetterIdException.class)
     public ApiResponse<?> handleNoLetterIdException(NoLetterIdException e) {
+        log.error(e.getMessage());
         return ApiResponse.onFailure(ErrorStatus.NOTIFICATION_NO_LETTER.getCode(), e.getMessage(), null);
     }
 
     @ExceptionHandler(NoTypeException.class)
     public ApiResponse<?> handleNoTypeException(NoTypeException e) {
+        log.error(e.getMessage());
         return ApiResponse.onFailure(ErrorStatus.NOTIFICATION_NO_TYPE.getCode(), e.getMessage(), null);
     }
 }
