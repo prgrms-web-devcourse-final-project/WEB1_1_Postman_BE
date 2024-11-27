dd5b87e1f2e843cd5acbb5aea409d3089129fc5d
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-11-27 17:20:18 +0900

    test: Label 서비스 테스트 - 특정 유저가 소유한 라벨 리스트를 반환한다.

--- src/test/java/postman/bottler/label/service/LabelServiceTest.java
+++ src/test/java/postman/bottler/label/service/LabelServiceTest.java
@@ -12,18 +12,23 @@ import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import postman.bottler.label.domain.Label;
 import postman.bottler.label.dto.response.LabelResponseDTO;
+import postman.bottler.user.infra.UserJpaRepository;
+
 
 @SpringBootTest
 class LabelServiceTest {
     @Mock
     private LabelRepository labelRepository;
 
+    @Mock
+    private UserJpaRepository userJpaRepository;
+
     @InjectMocks
     private LabelService labelService;
 
     @BeforeEach
     void setUp() {
-        reset(labelRepository);
+        reset(labelRepository, userJpaRepository);
     }
 
     @Test
@@ -57,5 +62,26 @@ class LabelServiceTest {
         assertEquals(2, dtos.size(), "DTO 리스트 사이즈는 2");
 
         verify(labelRepository, times(1)).findAllLabels();
+    }
+
+    @Test
+    @DisplayName("특정 유저가 소유한 라벨 리스트를 반환한다.")
+    void findUserLabels() {
+        // given
+        Long userId = 1L;
+        Label label1 = Label.createLabel("http://example.com/image1.png", 10);
+        Label label2 = Label.createLabel("http://example.com/image2.png", 10);
+        List<Label> labels = List.of(label1, label2);
+
+        when(labelRepository.findLabelsByUser(userId)).thenReturn(labels);
+
+        // when
+        List<LabelResponseDTO> dtos = labelService.findUserLabels(userId);
+
+        // then
+        assertNotNull(dtos, "Response DTO 리스트는 null이 될 수 없음");
+        assertEquals(2, dtos.size(), "DTO 리스트 사이즈는 2");
+
+        verify(labelRepository, times(1)).findLabelsByUser(userId);
     }
 }
