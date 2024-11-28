7f71f8d5bb85a5821f3f7a621da4e3c50d7c905e
l2yuPa <jeungwon28@gmail.com>
2024-11-29 03:41:58 +0900

    refactor: Getter 제거

--- src/main/java/postman/bottler/letter/exception/LetterNotFoundException.java
+++ src/main/java/postman/bottler/letter/exception/LetterNotFoundException.java
@@ -1,8 +1,5 @@
 package postman.bottler.letter.exception;
 
-import lombok.Getter;
-
-@Getter
 public class LetterNotFoundException extends RuntimeException {
 
     public LetterNotFoundException(String message) {
