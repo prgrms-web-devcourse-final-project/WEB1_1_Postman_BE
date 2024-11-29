74fb9bbf25e2a523b20c07fd968c869cdefdce24
ryu <jeungwon28@gmail.com>
2024-11-29 14:44:06 +0900

    hotfix: FCM Config 수정 2차

--- src/main/java/postman/bottler/config/FcmConfig.java
+++ src/main/java/postman/bottler/config/FcmConfig.java
@@ -7,6 +7,7 @@ import com.google.firebase.FirebaseOptions;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Base64;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,8 +22,11 @@ public class FcmConfig {
     public void init() {
         try {
             // JSON 문자열을 InputStream으로 변환
+            System.out.println("privateKey: " + privateKey);
             byte[] decodedBytes = Base64.getDecoder().decode(privateKey);
+            System.out.println("decodedBytes = " + Arrays.toString(decodedBytes));
             InputStream serviceAccount = new ByteArrayInputStream(decodedBytes);
+            System.out.println("serviceAccount = " + serviceAccount.getClass());
 
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
