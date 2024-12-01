55ee44f82643e1cab15c4c979bae86d9ea3d32c6
l2yuPa <jeungwon28@gmail.com>
2024-12-01 17:41:38 +0900

    feat: DeleteDTO 타입 재변경

--- src/main/java/postman/bottler/letter/dto/request/LetterDeleteRequestDTO.java
+++ src/main/java/postman/bottler/letter/dto/request/LetterDeleteRequestDTO.java
@@ -1,10 +1,12 @@
 package postman.bottler.letter.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import postman.bottler.letter.domain.BoxType;
+import postman.bottler.letter.domain.LetterType;
 
 public record LetterDeleteRequestDTO(
         @NotBlank(message = "Letter ID는 필수입니다.") Long letterId,
-        @NotBlank(message = "Letter Type은 필수입니다.") String letterType,
-        @NotBlank(message = "Box Type은 필수입니다.") String boxType
+        @NotBlank(message = "Letter Type은 필수입니다.") LetterType letterType,
+        @NotBlank(message = "Box Type은 필수입니다.") BoxType boxType
 ) {
 }
