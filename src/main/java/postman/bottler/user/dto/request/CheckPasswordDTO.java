6cd912d8181749a68f5baf88efa700e4c277631c
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-12-02 11:52:18 +0900

    feat: CheckPasswordDTO Valid 설정 추가

--- src/main/java/postman/bottler/user/dto/request/CheckPasswordDTO.java
+++ src/main/java/postman/bottler/user/dto/request/CheckPasswordDTO.java
@@ -1,6 +1,9 @@
 package postman.bottler.user.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record CheckPasswordDTO(
+        @NotBlank(message = "비밀번호는 필수 입력입니다.")
         String password
 ) {
 }
