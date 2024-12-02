9c1931fe0068634b3896805f7b9143893a2ba6ce
hgh1472 <hgh1472@naver.com>
2024-12-03 01:37:05 +0900

    hotfix: 조회된 도메인 객체를 JPA Entity로 재변환 시, createdAt이 null로 삽입되는 오류 수정

--- src/main/java/postman/bottler/complaint/domain/MapComplaint.java
+++ src/main/java/postman/bottler/complaint/domain/MapComplaint.java
@@ -1,5 +1,7 @@
 package postman.bottler.complaint.domain;
 
+import java.time.LocalDateTime;
+
 public class MapComplaint extends Complaint {
     private MapComplaint(Long letterId, Long reporterId, String description) {
         super(letterId, reporterId, description);
@@ -9,11 +11,12 @@ public class MapComplaint extends Complaint {
         return new MapComplaint(letterId, reporterId, description);
     }
 
-    private MapComplaint(Long id, Long letterId, Long reporterId, String description) {
-        super(id, letterId, reporterId, description);
+    private MapComplaint(Long id, Long letterId, Long reporterId, String description, LocalDateTime createdAt) {
+        super(id, letterId, reporterId, description, createdAt);
     }
 
-    public static MapComplaint of(Long id, Long letterId, Long reporterId, String description) {
-        return new MapComplaint(id, letterId, reporterId, description);
+    public static MapComplaint of(Long id, Long letterId, Long reporterId, String description,
+                                  LocalDateTime createdAt) {
+        return new MapComplaint(id, letterId, reporterId, description, createdAt);
     }
 }
