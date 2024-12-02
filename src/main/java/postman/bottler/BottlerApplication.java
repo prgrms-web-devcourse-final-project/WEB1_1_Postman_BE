d013a5faca193b3c8db5ffb97429fa91b65039ea
ryu <jeungwon28@gmail.com>
2024-12-02 19:45:56 +0900

    hotfix: ci 변경

--- src/main/java/postman/bottler/BottlerApplication.java
+++ src/main/java/postman/bottler/BottlerApplication.java
@@ -1,7 +1,5 @@
 package postman.bottler;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,14 +9,4 @@ public class BottlerApplication {
     public static void main(String[] args) {
         SpringApplication.run(BottlerApplication.class, args);
     }
-
-    @PostConstruct
-    public void loadEnv() {
-        Dotenv dotenv = Dotenv.load();
-
-        dotenv.entries().forEach(entry -> {
-            System.setProperty(entry.getKey(), entry.getValue());
-        });
-    }
-
 }
