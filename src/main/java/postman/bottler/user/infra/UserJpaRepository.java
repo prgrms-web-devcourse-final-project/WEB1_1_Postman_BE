8e34ffddae72cdf276dab83fc4f6688a15d14619
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-12-03 16:59:51 +0900

    refactor: 이메일 중복 확인 - 탈퇴한 유저 제외하고 확인

--- src/main/java/postman/bottler/user/infra/UserJpaRepository.java
+++ src/main/java/postman/bottler/user/infra/UserJpaRepository.java
@@ -2,10 +2,14 @@ package postman.bottler.user.infra;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import postman.bottler.user.infra.entity.UserEntity;
 
 public interface UserJpaRepository extends JpaRepository<UserEntity, Long>  {
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END " +
+            "FROM UserEntity u WHERE u.email = :email AND u.isDeleted = false")
     boolean existsByEmail(String email);
+
     boolean existsByNickname(String nickname);
     Optional<UserEntity> findByEmail(String email);
 }
