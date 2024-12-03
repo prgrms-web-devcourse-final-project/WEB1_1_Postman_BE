56ff9c0d523e1f6416e2a8f832456d4b60c6611e
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-12-03 15:30:28 +0900

    refactor: 유저 조회 시 유저 아이디 추가

--- src/main/java/postman/bottler/user/dto/response/UserResponseDTO.java
+++ src/main/java/postman/bottler/user/dto/response/UserResponseDTO.java
@@ -3,10 +3,11 @@ package postman.bottler.user.dto.response;
 import postman.bottler.user.domain.User;
 
 public record UserResponseDTO(
+        Long userId,
         String nickname,
         String profileImageUrl
 ) {
     public static UserResponseDTO from(User user) {
-        return new UserResponseDTO(user.getNickname(), user.getImageUrl());
+        return new UserResponseDTO(user.getUserId(), user.getNickname(), user.getImageUrl());
     }
 }
