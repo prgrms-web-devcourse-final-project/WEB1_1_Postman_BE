65a8fd399ba82f4cded7c2c15cb9790214934751
kahyun0255 <kkh9946@naver.com>
2024-12-03 12:08:38 +0900

    fix: #72 지도 편지 userDetails 붙이기

--- src/main/java/postman/bottler/config/SecurityConfig.java
+++ src/main/java/postman/bottler/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
                 .exceptionHandling(exceptionHandling -> exceptionHandling
                         .accessDeniedHandler(jwtAccessDeniedHandler)
