e14b2955e0f14f596ef145d9b6a65746dd490cf6
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-12-03 17:01:30 +0900

    refactor: 닉네임 중복 확인 - 탈퇴한 유저 제외하고 확인

--- src/main/java/postman/bottler/user/infra/UserJpaRepository.java
+++ src/main/java/postman/bottler/user/infra/UserJpaRepository.java
@@ -10,6 +10,9 @@ public interface UserJpaRepository extends JpaRepository<UserEntity, Long>  {
             "FROM UserEntity u WHERE u.email = :email AND u.isDeleted = false")
     boolean existsByEmail(String email);
 
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END " +
+            "FROM UserEntity u WHERE u.nickname = :nickname AND u.isDeleted = false")
     boolean existsByNickname(String nickname);
+
     Optional<UserEntity> findByEmail(String email);
 }
