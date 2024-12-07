bf0b5f26dd2bc2511d03572f70f660e9510bece2
hgh1472 <hgh1472@naver.com>
2024-12-07 22:12:23 +0900

    refactor: 알림 조회 HTTP 메서드 수정

--- src/main/java/postman/bottler/notification/controller/NotificationController.java
+++ src/main/java/postman/bottler/notification/controller/NotificationController.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -48,7 +49,7 @@ public class NotificationController {
     }
 
     @Operation(summary = "알림 조회", description = "사용자의 알림을 조회합니다.")
-    @GetMapping
+    @PatchMapping
     public ApiResponse<List<NotificationResponseDTO>> getNotifications(
             @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         List<NotificationResponseDTO> userNotifications = notificationService.getUserNotifications(
