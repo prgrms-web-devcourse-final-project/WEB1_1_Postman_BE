9c935840f671438b623bb733f44715d51f540192
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-12-09 18:20:56 +0900

    feat: 시간 설정 추가

--- src/main/java/postman/bottler/BottlerApplication.java
+++ src/main/java/postman/bottler/BottlerApplication.java
@@ -1,5 +1,7 @@
 package postman.bottler;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -7,6 +9,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class BottlerApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BottlerApplication.class, args);
