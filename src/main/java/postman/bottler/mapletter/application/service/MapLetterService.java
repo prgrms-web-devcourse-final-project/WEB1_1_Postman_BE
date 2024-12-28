817588681199a5b589275e46a5226d7128c5a2f5
kahyun0255 <kkh9946@naver.com>
2024-12-28 21:26:23 +0900

    fix: #235 보낸 답장 편지 조회시 페이지네이션 값이 없으면 totalElements가 0으로 가도록 수정

--- src/main/java/postman/bottler/mapletter/application/service/MapLetterService.java
+++ src/main/java/postman/bottler/mapletter/application/service/MapLetterService.java
@@ -296,6 +296,9 @@ public class MapLetterService {
         validMinPage(page);
         Page<ReplyMapLetter> letters = replyMapLetterRepository.findAllSentReplyByUserId(userId,
                 PageRequest.of(page - 1, size));
+        if(letters.isEmpty()){
+            return new PageImpl<>(Collections.emptyList(), PageRequest.of(page-1, size), 0);
+        }
         validMaxPage(letters.getTotalPages(), page);
 
         return letters.map(replyMapLetter -> {
