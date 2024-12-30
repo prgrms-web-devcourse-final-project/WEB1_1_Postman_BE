96f37ce2a56f9cbc47836db788e58a1bf55b44a6
hgh1472 <hgh1472@naver.com>
2024-12-31 00:02:25 +0900

    test: 알림 서비스 테스트 추가

--- src/test/java/postman/bottler/notification/application/NotificationServiceTest.java
+++ src/test/java/postman/bottler/notification/application/NotificationServiceTest.java
@@ -1,6 +1,7 @@
 package postman.bottler.notification.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.groups.Tuple.tuple;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -20,18 +21,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import postman.bottler.notification.application.dto.request.NotificationRequestDTO;
+import postman.bottler.notification.application.dto.request.RecommendNotificationRequestDTO;
 import postman.bottler.notification.application.dto.response.NotificationResponseDTO;
 import postman.bottler.notification.application.repository.NotificationRepository;
 import postman.bottler.notification.application.repository.SubscriptionRepository;
 import postman.bottler.notification.application.service.NotificationService;
 import postman.bottler.notification.domain.Notification;
+import postman.bottler.notification.domain.NotificationType;
 import postman.bottler.notification.domain.Notifications;
 import postman.bottler.notification.domain.Subscription;
 import postman.bottler.notification.domain.Subscriptions;
+import postman.bottler.notification.exception.NoLetterIdException;
 
 @DisplayName("알림 서비스 테스트")
 @ExtendWith(MockitoExtension.class)
@@ -124,6 +130,15 @@ public class NotificationServiceTest {
                     .containsExactlyInAnyOrder(KEYWORD_REPLY, 1L, 1L, "label");
         }
 
+        @DisplayName("편지 관련 알림에 편지 ID가 없는 경우, 예외가 발생한다.")
+        @ParameterizedTest
+        @CsvSource({"NEW_LETTER", "TARGET_LETTER", "MAP_REPLY", "KEYWORD_REPLY"})
+        void sendNotificationWithoutLetterId(NotificationType notificationType) {
+            // when then
+            assertThatThrownBy(() -> notificationService.sendNotification(notificationType, 1L, null, "label"))
+                    .isInstanceOf(NoLetterIdException.class);
+        }
+
         @Test
         @DisplayName("경고 알림을 보낸다.")
         public void sendWarningNotificationTest() {
@@ -144,7 +159,7 @@ public class NotificationServiceTest {
         }
 
         @Test
-        @DisplayName("경고 알림을 보낸다.")
+        @DisplayName("정지 알림을 보낸다.")
         public void sendBanNotificationTest() {
             // GIVEN
             Notification notification = Notification.create(BAN, 1L, null, null);
@@ -160,6 +175,26 @@ public class NotificationServiceTest {
             assertThat(response)
                     .extracting("type", "receiver", "letterId", "label")
                     .containsExactlyInAnyOrder(BAN, 1L, null, null);
+        }
+
+        @DisplayName("편지 관련 알림이 아닐 경우, 편지 ID와 라벨은 null로 저장된다.")
+        @ParameterizedTest
+        @CsvSource({"WARNING", "BAN"})
+        void sendNonLetterNotificationWithLabelAndLetterId(NotificationType notificationType) {
+            // given
+            Notification notification = Notification.create(notificationType, 1L, 1L, "label");
+
+            given(notificationRepository.save(any())).willReturn(notification);
+            given(subscriptionRepository.findByUserId(1L))
+                    .willReturn(Subscriptions.from(List.of(Subscription.create(1L, "token"))));
+
+            // when
+            NotificationResponseDTO response = notificationService.sendNotification(BAN, 1L, 1L, "label");
+
+            // then
+            assertThat(response)
+                    .extracting("letterId", "label")
+                    .containsExactlyInAnyOrder(null, null);
         }
 
         @Test
@@ -208,6 +243,30 @@ public class NotificationServiceTest {
                             tuple(1L, NEW_LETTER),
                             tuple(1L, TARGET_LETTER),
                             tuple(1L, MAP_REPLY));
+        }
+    }
+
+    @Nested
+    @DisplayName("편지 추천 알림")
+    class Recommendation {
+
+        @DisplayName("추천된 편지를 모든 유저에게 알림을 전송한다.")
+        @Test
+        void sendKeywordNotifications() {
+            // given
+            List<RecommendNotificationRequestDTO> recommends = List.of(
+                    new RecommendNotificationRequestDTO(1L, 1L, "label"),
+                    new RecommendNotificationRequestDTO(2L, 1L, "label"));
+
+            given(subscriptionRepository.findAll()).willReturn(
+                    Subscriptions.from(List.of(Subscription.create(1L, "token"), Subscription.create(2L, "token"))));
+
+            // when
+            notificationService.sendKeywordNotifications(recommends);
+
+            // then
+            verify(notificationRepository, times(2)).save(any());
+            verify(pushNotificationProvider, times(1)).pushAll(any());
         }
     }
 }
