b7a8a7846be7faa4a982ef6b24d33740ab5ebf5d
l2yuPa <jeungwon28@gmail.com>
2025-01-24 22:13:29 +0900

    refactor: 커스텀 예외 Enum과 AOP를 활용하여 개선

--- src/main/java/postman/bottler/letter/exception/InvalidDeleteRequestException.java
+++
@@ -1,9 +0,0 @@
-package postman.bottler.letter.exception;
-
-import java.util.Map;
-
-public class InvalidDeleteRequestException extends BaseLetterValidationException {
-    public InvalidDeleteRequestException(String message, Map<String, String> errors) {
-        super(message, errors);
-    }
-}
