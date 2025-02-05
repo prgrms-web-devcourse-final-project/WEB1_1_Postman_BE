ebf03c6f02229e9ee1e873b163006fc57e715402
l2yuPa <jeungwon28@gmail.com>
2025-02-05 13:06:30 +0900

    feat: LetterCustomException 적용 및 message LetterType으로 구분

--- src/main/java/postman/bottler/letter/exception/LetterNotFoundException.java
+++ src/main/java/postman/bottler/letter/exception/LetterNotFoundException.java
@@ -1,8 +1,24 @@
 package postman.bottler.letter.exception;
 
-public class LetterNotFoundException extends RuntimeException {
+import static postman.bottler.global.response.code.ErrorStatus.LETTER_NOT_FOUND;
 
-    public LetterNotFoundException(String message) {
-        super(message);
+import postman.bottler.letter.domain.LetterType;
+
+public class LetterNotFoundException extends LetterCustomException {
+
+    public LetterNotFoundException() {
+        super(LETTER_NOT_FOUND, "편지가 존재하지 않습니다.");
+    }
+
+    public LetterNotFoundException(LetterType letterType) {
+        super(LETTER_NOT_FOUND, getMessageByLetterType(letterType));
+    }
+
+    private static String getMessageByLetterType(LetterType letterType) {
+        return switch (letterType) {
+            case LETTER -> "키워드 편지가 존재하지 않습니다.";
+            case REPLY_LETTER -> "키워드 답장 편지가 존재하지 않습니다.";
+            case UNKNOWN -> "편지가 존재하지 않습니다.";
+        };
     }
 }
